ab7636e1907124e8fb68bfcec6bfccb937ec9688
Shu Yang <shu_1315@hotmail.com>
2026-05-15 21:38:09 -0700

    edits according to code rabbit

--- db/migrations/006_migrate_district_type_place_and_school_variants.sql
+++ db/migrations/006_migrate_district_type_place_and_school_variants.sql
@@ -29,6 +29,7 @@ ALTER TABLE districts
   ADD CONSTRAINT chk_district_type
   CHECK (
     district_type IN (
+      'us_senate',
       'statewide',
       'us_house',
       'state_upper',
@@ -45,6 +46,7 @@ ALTER TABLE user_districts
   ADD CONSTRAINT chk_user_districts_type
   CHECK (
     district_type IN (
+      'us_senate',
       'statewide',
       'us_house',
       'state_upper',
--- db/migrations/008_rename_district_type_place_to_incorporated_place.sql
+++ db/migrations/008_rename_district_type_place_to_incorporated_place.sql
@@ -21,6 +21,7 @@ ALTER TABLE districts
   ADD CONSTRAINT chk_district_type
   CHECK (
     district_type IN (
+      'us_senate',
       'statewide',
       'us_house',
       'state_upper',
@@ -37,6 +38,7 @@ ALTER TABLE user_districts
   ADD CONSTRAINT chk_user_districts_type
   CHECK (
     district_type IN (
+      'us_senate',
       'statewide',
       'us_house',
       'state_upper',
